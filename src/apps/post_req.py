fba729ecb143caa8c819a61da04d65669e97abb4
Ubuntu <ubuntu@ip-172-31-12-193.us-west-2.compute.internal>
2024-04-05 07:09:09 +0000

    minor update

--- src/apps/post_req.py
+++ src/apps/post_req.py
@@ -1,7 +1,9 @@
-from pgdriver import pgDriver
+from pgdriver import pgConnector
+from datetime import datetime
 import subprocess
 import json
 import requests
+import pdb
 
 def get_EC2_public_address(instance):
     acmd = f"aws ec2 describe-instances --filters \"Name=tag:Name,Values={instance}\" --query 'Reservations[*].Instances[*].PublicDnsName' --output text"
@@ -10,16 +12,27 @@ def get_EC2_public_address(instance):
     return output.decode().splitlines()[0]
 
 house_id=4340
-payload = {'net' : [], 'irrad' : []}
-pgh = pgDriver()
+# what else we need to put in the request???
+payload = {'house_id' : house_id, 'net' : [], 'irrad' : []}
+pgh = pgConnector()
 
 # generate payload first using historical data in PSQL
-query = f"select nl.year,nl.month,nl.day,nl.timestamp,nl.load_value from net_load_ts nl, smartmeter m, house h where nl.meter_id=m.meter_id and m.house_id = h.house_id and h.house_id={house_id} order by (year,month,day,timestamp) desc limit 192"
+# Disaggregation: provided timestamp - 192 steps sliding window
+# Classifcation: provided timestamp - 96 - extract full 96 steps on that day
+timestamp = '2018-11-23 05:15'
+timeobj = datetime.strptime(timestamp, "%Y-%m-%d %H:%M")
+
+query = f"select nl.year,nl.month,nl.day,nl.timestamp,nl.load_value from net_load_ts nl, smartmeter m, house h where nl.meter_id=m.meter_id and m.house_id = h.house_id and h.house_id=4340 and nl.timestamp < '{timestamp}' order by timestamp desc limit 192"
+
 data = pgh._read(query)
 payload['net'] = [i[4] for i in reversed(data)]
-query = f"select w.year, w.month, w.day, w.timestamp, w.irradiance_value from weather w, smartmeter m, smartmeter_weather_map swm, house h where w.weather_id = swm.weather_id and m.meter_id = swm.meter_id and m.house_id = h.house_id and h.house_id={house_id} order by (year, month, day, timestamp) desc limit 192"
+query = f"select w.year, w.month, w.day, w.timestamp, w.irradiance_value from weather w, smartmeter m, smartmeter_weather_map swm, house h where w.weather_id = swm.weather_id and m.meter_id = swm.meter_id and m.house_id = h.house_id and h.house_id={house_id} and w.timestamp < '{timestamp}' order by timestamp desc limit 192"
 data = pgh._read(query)
 payload['irrad'] = [i[4] for i in reversed(data)]
+
+query = f"select nl.year,nl.month,nl.day,nl.timestamp,nl.load_value from net_load_ts nl, smartmeter m, house h where nl.meter_id=m.meter_id and m.house_id = h.house_id and h.house_id=4340 and nl.year={timeobj.year} and nl.month={timeobj.month} and nl.day = {timeobj.day - 1} order by timestamp"
+data = pgh._read(query)
+payload['clf_net'] = [i[4] for i in data]
 
 print(payload)
 headers = {'Content-Type': 'application/json'}
