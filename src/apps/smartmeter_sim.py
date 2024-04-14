031792c179ce06316e276c28da42a7a4bca29a40
Ubuntu <ubuntu@ip-172-31-12-193.us-west-2.compute.internal>
2024-04-14 19:25:53 +0000

    minor update for demo

--- src/apps/smartmeter_sim.py
+++ src/apps/smartmeter_sim.py
@@ -16,7 +16,7 @@ def get_EC2_public_address(instance):
 
 def get_irradiance_sequence(idata, day, hour, minute):
     if day not in idata:
-        print(f"Day not found in irradiance data: {day}")
+        print(f"Day not found in irradiance data: {day} {hour} {minute}")
         return 0
 
     #print(f"irradance size: {len(idata[day])} on day [{day}]")
@@ -37,7 +37,7 @@ pgh = pgConnector(transaction=True)
 
 df = pd.read_csv("/opt/data/pecan/pecan_meta.csv")
 houses = list(set(df['house_id']))
-print(houses)
+#print(houses)
 headers = {'Content-Type': 'application/json'}
 port = 20100
 app_host = get_EC2_public_address("csprod-infra-01")
@@ -46,11 +46,13 @@ url = f"http://{app_host}:{port}/getRadianceData"
 # use startdate and duration
 #houses = [3687, 9160]
 houses = [
-#387, 1417, 142, 2318, 914, 27, 3996, 3488, 5679, 2096, 558, 2358, 950, 3000, 4283, 3517, 5058, 4550, 1222, 5587, 1240, 9053, 5982, 5997, 3700
-4550, 1222, 5587, 1240, 9053, 5982, 5997, 3700
+#203, 1222, 3039, 2606, 8386, 2818, 3864, 8565, 4031, 4495, 4934, 5679, 8733, 9160, 9019, 9213, 2358, 3488, 9278, 5997, 3700, 3996, 9612, 9775, 27, 9836, 9922, 4283, 3687, 2318, 914, 1450, 1524, 2096, 1642, 142, 5587, 1240, 387, 1417, 5058, 9053, 2335, 3456, 4767, 2361, 3538, 4373, 5746, 6139
+#387, 1417, 142, 2318, 914, 3996, 3488, 5679, 2096, 558, 2358, 950, 3000, 4283, 3517, 5058, 4550, 1222, 5587, 1240, 9053, 5982, 5997, 3700
+#6377, 6547, 661, 7536, 7062, 7719, 7901, 7951, 8156, 8342,
+#1731, 558, 5982, 3517, 950, 4550, 3000
+7800
 ]
 
-pdb.set_trace()
 for h in houses:
    # get raw time series data from Pecan
    house_data = pd.read_csv(f"/opt/data/pecan/{h}.csv")
@@ -85,7 +87,7 @@ for h in houses:
       print(f"Processing timestamp [{full_ts}]...")
       hm_ts = timeobj.strftime('%H:%M')
       #if not (mon == 1 and day in [1,2]):
-      if not (mon == 5 and day in [1,2]):
+      if not (mon == 1 and day in [1,2]):
          # get the net_load and irradiance data
          #time.sleep(1)
          payload['house_id'] = h
@@ -110,9 +112,9 @@ for h in houses:
          murl = f"http://{app_host}:{port}/predict"
          resp = requests.post(murl, data=json.dumps(payload), headers=headers)
          if resp.status_code == 200:
-             print(f"Prediction request posted successfully to [{murl}]")
+             print(f"[H:{h}, M:{meter_id}] Prediction request posted successfully to [{murl}]")
          else:
-             print(f"Prediction request failed [{murl}]")
+             print(f"[H:{h}, M:{meter_id}] Prediction request failed [{murl}]")
 
       # insert net_load
       query = f"insert into net_load_ts (meter_id, month, day, year, h_timestamp, timestamp, load_value) values ({meter_id}, {mon}, {day}, {year}, '{hm_ts}', '{full_ts}', {entry['grid']}) returning *"
@@ -123,7 +125,7 @@ for h in houses:
          pass
 
       # insert irradiance/weather
-      ghi = get_irradiance_sequence(irradiance, f"{year}-{mon}-{day}", hour, mins)
+      ghi = get_irradiance_sequence(irradiance, f"{year}-{mon:02d}-{day:02d}", hour, mins)
       query = f"insert into weather (month, day, year, h_timestamp, timestamp, irradiance_value) values ({mon}, {day}, {year}, '{hm_ts}', '{full_ts}', {ghi}) returning *"
       #print(f"{h} {query}")
       wid = 0
